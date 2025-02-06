165be534924d91e97e30db7e9a96445d0714e267
Salatiel Martinez <salatiel@umich.edu>
2025-02-06 14:20:32 -0500

    Added different print text

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public class Hello {
         public static void main(String[] args) {
         for(int i=1;i<=10;i++) {
-        System.out.println("Hello World..."+i);
+        System.out.println("I want you to be mine again baby aye"+i);
         }
         }
         }
