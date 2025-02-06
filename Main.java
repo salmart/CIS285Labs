d2e24b9ca35131a51f489c26db231328ccd104a6
Salatiel Martinez <salatiel@umich.edu>
2025-02-06 14:46:33 -0500

    Task2 Step5

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         public static void main(String[] args) {
         for(int i=1;i<=10;i++) {
         System.out.println("I want you to be mine again baby aye"+i);
+        System.out.println("this is afet the jenkins creation of a project")
         }
         }
         }
